626f3cb87e1f886a9f8cf539f061bfa14cafa9c2
Ada Tur <47090586+AdaDTur@users.noreply.github.com>
2023-01-14 18:13:04 -0500

    Update CheckPrime.ml

--- Code Saves/CheckPrime.ml
+++ Code Saves/CheckPrime.ml
@@ -1,15 +1,3 @@
-# Lecture 3
-
-# Recursion and Tail Calls
-
-- CPU doesn’t know what a function is, just executes a series of instructions
-- Call Stack is memory that keeps track of function calls
-    - Pushes current instruction address on stack
-    - Pushes arguments
-    - Jump to code of function
-    - To return: pop all arguments and return address
-
-```ocaml
 let is_prime n =
   if n < 2 then failwith "no can do" else
     let rec go (i : int) : bool = 
@@ -26,27 +14,5 @@ let is_prime n =
         go (n+1)
     in
     go 2
-```
 
-- Tracing shows every step of computation until value is reached
-    - End up with long stack frame
-        - 5 + (4 + (3 + (2 + (1 + sum(0)))))
-    - Instead, tail calls
-        - Recycles stack frames
-- **Tail call optimization** (TCO) recycled stack frame of current function
-- TCO can be done on any recursive algorithm, and enablese running in **constant stack space**
-    - Often introduce extra parameter, called **************accumulator**************
-- Tail-recursive
 
-```ocaml
-let rec sum n partial_sum =
-  if n = 0 then partial_sum else sum (n-1) (partial_sum + n)
-```
-
-- Type synonyms
-    - type name = string
-    - type height = int
-- Tuples
-    - Collection of values of various types
-    - type person = name * height
-    - let ada : person = (”ada”, 100)
