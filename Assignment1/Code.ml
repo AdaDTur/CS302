275699b49a1f187ee3566e34a49d401416433c87
Ada Tur <47090586+AdaDTur@users.noreply.github.com>
2023-01-14 21:07:26 -0500

    Update Code.ml

--- Assignment1/Code.ml
+++ Assignment1/Code.ml
@@ -9,10 +9,9 @@ let distance_tests = [
          the correct answers.
 *)
 let distance (x1, y1) (x2, y2) =
-  if x1 < x2 & y1 < y2 then (x2 - x1) + (y2 - y1) else
-  if x1 > x2 & y1 > y2 then (x1 - x2) + (y1 - y2) else
-  if x1 > x2 & y1 < y2 then (x1 - x2) + (y2 - y1) else
-  if x1 < x2 & y1 > y2 then (x2 - x1) + (y1 - y2) else 0;;
+  if x1 < x2 && y1 < y2 then (x2 - x1) + (y2 - y1) else
+  if x1 > x2 && y1 > y2 then (x1 - x2) + (y1 - y2) else
+  if x1 > x2 && y1 < y2 then (x1 - x2) + (y2 - y1) else (x2 - x1) + (y1 - y2) ;;
 
 
 
@@ -29,10 +28,15 @@ let binomial_tests = [
 
 (* TODO: Correct this implementation so that it compiles and returns
          the correct answers.
-*)
+*) 
+let factorial num =
+  let rec go j acc =
+    if j > 0 then go (j - 1) (acc * j) else acc
+  in
+  go num 1
+    
 let binomial n k =
-  raise NotImplemented
-
+  if (k != 0 && (n - k) != 0) then (factorial n) / ((factorial k) * (factorial (n - k))) else 0
 
 
 (* Question 3: Lucas Numbers *)
