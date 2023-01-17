7909f40fe43920e4cb7bbac779364f390fe0c307
Ada Tur <47090586+AdaDTur@users.noreply.github.com>
2023-01-17 11:31:05 -0500

    Update Code.ml

--- Assignment1/Code.ml
+++ Assignment1/Code.ml
@@ -3,10 +3,10 @@
 let distance_tests = [ 
   (((1, 2), (2, 4)), 3);
   (((-1, 0), (5, 0)), 6);
-  (((0, 0), (6, 1)), 7);
+  (((-1, -2), (-2, -1)), 2);
   (((1, 8), (2, 4)), 5);
   (((10, 10), (0, 0)), 20);
-  (((1, 1), (2, 2)), 2)
+  (((0, 0), (0, 0)), 0)
 ]
 ;;
 
@@ -14,8 +14,8 @@ let distance_tests = [
          the correct answers.
 *)
 let distance (x1, y1) (x2, y2) =
-  if x1 < x2 && y1 < y2 then (x2 - x1) + (y2 - y1) else
-  if x1 > x2 && y1 > y2 then (x1 - x2) + (y1 - y2) else
+  if x1 <= x2 && y1 <= y2 then (x2 - x1) + (y2 - y1) else
+  if x1 >= x2 && y1 >= y2 then (x1 - x2) + (y1 - y2) else
   if x1 > x2 && y1 < y2 then (x1 - x2) + (y2 - y1) else (x2 - x1) + (y1 - y2) ;;
 
 
@@ -27,21 +27,18 @@ let distance (x1, y1) (x2, y2) =
 *)
 let binomial_tests = [
   (* Your test cases go here. Correct the incorrect test cases for the function. *)
-  ((0, 0), 0);
-  ((0, 0), 0);
-  ((0, 0), 0);
-  ((0, 0), 0);
+  ((0, 0), 1);
+  ((1, 2), 0);
+  ((-4, 0), 1);
+  ((0, 0), 1);
 
 ]
 
 (* TODO: Correct this implementation so that it compiles and returns
          the correct answers.
 *) 
-let factorial num =
-  let rec go j acc =
-    if j > 0 then go (j - 1) (acc * j) else acc
-  in
-  go num 1
+let rec factorial num =
+  if num <= 1 then 1 else factorial (num-1) * num
     
 let binomial n k =
   (factorial n) / ((factorial k) * (factorial (n - k)))
@@ -50,15 +47,19 @@ let binomial n k =
 
 (* TODO: Write a good set of tests for lucas_tests. *)
 let lucas_tests = [
-  
+  ((1), 1);
+  ((0), 2);
+  ((3), 4);
+  ((5), 11);
+  ((7), 29);
 ]
 
 (* TODO: Implement a tail-recursive helper lucas_helper. *)
-let rec lucas_helper n m acc =
-  if n <= 0 then 2 else
-  if n = 1 then 1 else lucas_helper (n - 1) m (acc + m)
+let rec lucas_helper n a b =
+  if n = 0 then a else lucas_helper (n-1) b (a+b)
+    
 
 
 (* TODO: Implement lucas that calls the previous function. *)
 let lucas n = 
-  lucas_helper n 0 1
+  lucas_helper n 2 1
